08a8532efca04f511136de5958feb79f8cdb896f
Tim Uy <tim@loqu8.com>
2014-10-21 10:48:05 -0700

    Initial project

---
+++ src/Loqu8.Onyx/Sampler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Loqu8.Onyx
+{
+    public class Sampler
+    {
+        public Guid NewId
+        {
+            get
+            {
+                return Guid.NewGuid();
+            }
+        }
+    }
+}
